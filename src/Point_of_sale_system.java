8b27e0545da292ae7bad15d38d13e28c200d6a1d
Rashmi Dahal <Rashmi.Dahal1@veritas.com>
2019-05-15 11:38:05 -0500

    Added Bobby's changes back

--- src/Point_of_sale_system.java
+++ src/Point_of_sale_system.java
@@ -1,5 +1,16 @@
 package src;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 import java.util.function.UnaryOperator;
 
 import org.omg.CORBA.portable.ValueFactory;
@@ -223,8 +234,84 @@ public class Point_of_sale_system extends Application {
 		        		view.balanceAmountLabel.setText(Double.toString(balance));
 		        	}
 		        }
+		        
+		       Date date = new Date();
+		       SimpleDateFormat ft = new SimpleDateFormat("E yyyy.MM.dd 'at' hh:mm:ss a zzz");
+               String overAllPrice = view.totalPriceAmountLabel.getText();
+               //double n = Math.random();
+               //long n1 = Math.round(Math.random()*10);
+               int n1 = 1;
+               long n6 = Math.round(Math.random()*1000000);
+               String cashierName = view.nameField.getText();
+
+               File directory = new File("C:\\Drawer");
+               if (! directory.exists()){
+            	   directory.mkdir();
+               }
+
+               String drawerData;
+               if (cashierName.equals("bvang")) {
+            	   	drawerData = "C:\\Drawer\\bvang_drawer.csv";
+               } else if (cashierName.equals("rdahal")) {
+            	   drawerData = "C:\\Drawer\\rdahal_drawer.csv";
+               } else if (cashierName.equals("jlie")) {
+            	   drawerData = "C:\\Drawer\\jlie_drawer.csv";
+               } else {
+            	   drawerData = "C:\\Drawer\\drawer.csv";
+               }
+
+               try (BufferedWriter bf = new BufferedWriter(new FileWriter(drawerData, true)))
+               {
+            	   bf.newLine();
+            	   bf.write("*************Point Of Sale System*************,");
+            	   bf.newLine();
+            	   bf.write(ft.format(date));
+            	   bf.newLine();
+            	   bf.write("Register ID: " + n1);
+            	   bf.newLine();
+            	   bf.write("Cashier Name: " + cashierName);
+            	   bf.newLine();
+            	   bf.write("Sale ID: " + n6);
+            	   bf.newLine();
+            	   bf.write("Total Sales: $" + overAllPrice);
+            	   bf.newLine();
+            	   bf.close();
+               }
+               catch (IOException ex)
+               {
+
+               }
+
+               File totalAmountOfSale = new File("C:\\Drawer\\totalAmountOfSales.txt");
+               try (BufferedWriter df = new BufferedWriter(new FileWriter(totalAmountOfSale, true)))
+               {
+            	   df.write(overAllPrice);
+            	   df.newLine();
+            	   df.close();
+               }
+               catch (IOException ex)
+               {
+            	   //
+               }
 
 			}
+	    });
+	    
+	  //bind export button to output inventory csv
+	    view.export.setOnAction(new EventHandler<ActionEvent>() {
+	    	@Override
+	    	public void handle(ActionEvent e)
+	    	{
+	    		try
+	    		{
+	    			writeExcel();
+	    		}
+	    		catch (Exception ex)
+	    		{
+	    			ex.printStackTrace();
+	    		}
+	    	}
+
 	    });
 	   	   
 	}
@@ -279,6 +366,29 @@ public class Point_of_sale_system extends Application {
 		  //bind the totalPrice to totalPriceLabel
 			view.totalPriceAmountLabel.textProperty().bind(Bindings.format("%.2f", totalPriceAmountLabelBinding));
 	 }
+	 
+	 public void writeExcel() throws Exception {
+		Writer writer = null;
+		try {
+			File file = new File("src/Inventory.csv");
+			writer = new BufferedWriter(new FileWriter(file));
+	
+	
+			for (Item r : view.tableView.getItems()) {
+	
+				String text = r.getName() + "," + "Quantity: " + r.getUnitQuantity() + "," + "Threshold: 5" + "," + "Supplier: Cub Foods" + "," + "Outstanding Order: 5" + "\n";
+	
+				writer.write(text);
+			}
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		finally {
+	
+			writer.flush();
+			writer.close();
+		}
+	}
 	 	 
 }
 
